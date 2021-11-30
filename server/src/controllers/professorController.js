8c3622e5623c7f33bb12aae02a437a806885923d
thmartins27 <thiago.olivmartins@gmail.com>
2021-11-30 17:44:36 -0300

    manuntenção em professorController.js

--- server/src/controllers/professorController.js
+++ server/src/controllers/professorController.js
@@ -5,12 +5,16 @@ module.exports = {
         let json = {erro: '', results: []}
         try{
             let professor = await professorService.buscarTodos()
-            for(i in professor){
-                json.results.push({
-                    codigo: professor[i].cod_professor,
-                    nome: professor[i].nome,
-                    sobrenome: professor[i].sobrenome
-                })
+            if(professor[0]){
+                for(i in professor){
+                    json.results.push({
+                        codigo: professor[i].cod_professor,
+                        nome: professor[i].nome,
+                        sobrenome: professor[i].sobrenome
+                    })
+                }
+            }else{
+                json.erro = 'Nenhum dado encontrado'
             }
         }catch(e){
             json.erro = `Erro de query: ${e}`
@@ -24,8 +28,14 @@ module.exports = {
         let codigo = req.params.codigo
         try{
             let professor = await professorService.buscarUm(codigo)
-            if(professor){
-                json.results = professor
+            if(professor[0]){
+                json.results = {
+                    codigo: professor[0].cod_professor,
+                    nome: professor[0].nome,
+                    sobrenome: professor[0].sobrenome
+                }
+            }else{
+                json.erro = `${codigo} não encontrado`
             }
         }catch(e){
             json.erro = `Erro de query: ${e}`
@@ -42,15 +52,13 @@ module.exports = {
         try{
             if(codigo && nome && sobrenome){
                 let professor = await professorService.addProfessor(codigo, nome, sobrenome)
-    
-                json.results = {
-                    id: professor,
-                    codigo,
-                    nome,
-                    sobrenome
+                if(professor.affectedRows == 1){
+                    json.results = `Professor ${codigo} adicionado com sucesso`
+                }else{
+                    json.erro = 'Erro ao cadastrar professor'
                 }
             }else{
-                json.erro = 'Campos não enviados'
+                json.erro = 'Campos não preenchidos'
             }
         }catch(e){
             json.erro = `Erro de query: ${e}`
@@ -60,24 +68,27 @@ module.exports = {
     },
 
     alterProfessor: async(req, res) => {
-        let json = {erro: '', results: {}}
+        let json = {erro: '', results: ''}
         let codigo = req.params.codigo
         let nome = req.body.nome
         let sobrenome = req.body.sobrenome
-        try{
-            let professor = await professorService.alterProfessor(codigo, nome, sobrenome)
-            if(professor){
-                json.results = professor
-            }else{
-                json.erro = 'Campos não enviados'
+        
+        if(nome && sobrenome){
+            try{
+                let aluno = await professorService.alterProfessor(codigo, nome, sobrenome)
+                if(aluno.affectedRows == 1){
+                    json.results = 'Dados alterados com sucesso'
+                }else{
+                    json.erro = 'Nenhum dado foi alterado'
+                }
+            }catch(e){
+                json.erro = `Erro de query: ${e}`
+                console.log(json.erro)
             }
-        }catch(e){
-            json.erro = `Erro de query: ${e}`
-            console.log(json.erro)
+        }else{
+            json.erro = 'Campos não enviados'
         }
-
         res.json(json)
-
     },
 
     delete: async(req, res) => {
